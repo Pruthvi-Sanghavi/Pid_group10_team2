1708df5460799062af742bd59b53eddb3f977992
Pruthvi <45816639+Pruthvi-Sanghavi@users.noreply.github.com>
2019-09-26 12:31:04 -0400

    Update and rename test.cpp to PIDControllerTest.cpp

--- test/PIDControllerTest.cpp
+++ test/PIDControllerTest.cpp
@@ -1,5 +1,5 @@
 /*@ 
-/@file test.cpp
+/@file PIDControllerTest.cpp
 /@author Ryan Bates, Pruthvikumar Sanghavi
 /@brief This is the test class
 /@copyright 2019 Ryan Bates, Pruthvikumar Sanghavi
@@ -9,19 +9,19 @@
 // @ This is a very very simple test
 TEST(trivialcomputetest, should_pass) {
   PIDController controller(0,0,0,0,0,0);
-  EXPECT_DOUBLE_EQ(controller.compute(1,0),0);
+  EXPECT_DOUBLE_EQ(0,controller.compute(1,0));
   
 }
 // @ This is a simple test
 TEST(simplecomputetest, should_pass) {
   PIDController controller(1.0,0.0,1.0,2.0,1.5,2.5);
-  EXPECT_DOUBLE_EQ(controller.compute(1,0),-0.5);
+  EXPECT_DOUBLE_EQ(-0.5,controller.compute(1,0));
   
 }
 // @ This is a complex test
 TEST(computetest, should_pass) {
   PIDController controller(1.0,1.0,1.0,2.0,1.5,2.5);
-  EXPECT_DOUBLE_EQ(controller.compute(1,0),2.0);
+  EXPECT_DOUBLE_EQ(2.0,controller.compute(1,0));
   
 }
 
